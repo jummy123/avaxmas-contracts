54620ff54f82d7fe912003c6d41b71fc90ae1f8a
jummy123 <jummy123@users.noreply.github.com>
2022-12-23 16:22:12 +0000

    Tweak allow list script

--- scripts/allowList.js
+++ scripts/allowList.js
@@ -2,6 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 const fs = require('fs');
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 task("allowListCollections", "Allow lists collections from a JSON file'")
   .addParam("path", "The path to json file of collections!")
@@ -14,6 +19,7 @@ task("allowListCollections", "Allow lists collections from a JSON file'")
     });
     for (let i=0; i < collectionAddresses.length; i+= 100) {
         await secretSanta.allowListCollections(collectionAddresses.slice(i, i+100));
+        await sleep(2000);
         console.log('allow listed contracts', i, i+100);
     }
   });
